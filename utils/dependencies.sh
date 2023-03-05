bdea7eede69c3cb6e23aa8fe9261f2729643b9d1
mogoll92 <mogoll92@gmail.com>
2023-03-05 12:59:15 +0200

    Added dependencies installation

---
+++ utils/dependencies.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Installing dependencies..." && sleep 1
+sudo apt update &&
+sudo apt install -y curl iptables build-essential git wget jq make gcc nano \
+tmux htop nvme-cli pkg-config libssl-dev libleveldb-dev tar clang bsdmainutils ncdu unzip libleveldb-dev
+
+echo "Installing go..." && sleep 1
+if ! [ -x "$(command -v go)" ]; then
+  source <(curl -s "https://raw.githubusercontent.com/R1M-NODES/cosmos/master/utils/install_go.sh")
+  source .bash_profile
+fi
+
+echo "$(go version)"
