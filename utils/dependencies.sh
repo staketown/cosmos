b35e35a6b9eb91e84e1442e1a19e67b6be60f9e8
mogoll92 <mogoll92@gmail.com>
2023-03-05 15:17:08 +0200

    Updated nibiru script

--- utils/dependencies.sh
+++ utils/dependencies.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Installing dependencies..." && sleep 1
-sudo apt update &&
-sudo apt install -y curl iptables build-essential git lz4 wget jq make gcc nano chrony \
-tmux htop nvme-cli pkg-config libssl-dev libleveldb-dev tar clang bsdmainutils ncdu unzip libleveldb-dev
+sudo apt-get update &&
+sudo apt-get install -y curl iptables build-essential git lz4 wget jq make gcc nano chrony \
+tmux htop nvme-cli pkg-config libssl-dev libleveldb-dev tar clang bsdmainutils ncdu unzip libleveldb-dev &&
 
 echo "Installing go..." && sleep 1
 if ! [ -x "$(command -v go)" ]; then
