d63f3f55a88f20feb470ae0dc6b0f30e66e42d39
mogoll92 <mogoll92@gmail.com>
2023-03-05 15:03:27 +0200

    Updated nibiru script

--- utils/dependencies.sh
+++ utils/dependencies.sh
@@ -2,13 +2,13 @@
 
 echo "Installing dependencies..." && sleep 1
 sudo apt update &&
-sudo apt install -y curl iptables build-essential git lz4 wget jq make gcc nano \
+sudo apt install -y curl iptables build-essential git lz4 wget jq make gcc nano chrony \
 tmux htop nvme-cli pkg-config libssl-dev libleveldb-dev tar clang bsdmainutils ncdu unzip libleveldb-dev
 
 echo "Installing go..." && sleep 1
 if ! [ -x "$(command -v go)" ]; then
-  source <(curl -s "https://raw.githubusercontent.com/R1M-NODES/cosmos/master/utils/install_go.sh")
-  source .bash_profile
+  source <(curl -s "https://raw.githubusercontent.com/R1M-NODES/cosmos/master/utils/go_install.sh")
+  source $HOME/.bash_profile
 fi
 
 echo "$(go version)"
