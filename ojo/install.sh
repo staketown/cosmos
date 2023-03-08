7e713b3009c9b836bfe9a44e8c2573c645079742
mogoll92 <mogoll92@gmail.com>
2023-03-08 15:47:16 +0200

    Added port support

--- ojo/install.sh
+++ ojo/install.sh
@@ -5,8 +5,7 @@ source <(curl -s https://raw.githubusercontent.com/R1M-NODES/cosmos/master/utils
 printLogo
 
 source <(curl -s https://raw.githubusercontent.com/R1M-NODES/cosmos/master/utils/ports.sh) && sleep 1
-
-selectPortSet
+export -f selectPortSet
 
 read -r -p "Enter node moniker: " NODE_MONIKER
 
