61b87b36e8eed285175e786b3dd1dd07ad1edc63
mogoll92 <mogoll92@gmail.com>
2023-03-09 23:39:54 +0200

    Added lava install

--- lava/install.sh
+++ lava/install.sh
@@ -9,10 +9,10 @@ export -f selectPortSet && selectPortSet
 
 read -r -p "Enter node moniker: " NODE_MONIKER
 
-CHAIN_ID="nibiru-itn-1"
-CHAIN_DENOM="unibi"
-BINARY_NAME="nibid"
-BINARY_VERSION_TAG="v0.19.2"
+CHAIN_ID="lava-testnet-1"
+CHAIN_DENOM="ulava"
+BINARY_NAME="lavad"
+BINARY_VERSION_TAG="v0.7.0"
 CHEAT_SHEET=""
 
 printDelimiter
@@ -27,27 +27,27 @@ source <(curl -s https://raw.githubusercontent.com/R1M-NODES/cosmos/master/utils
 echo "Building binaries..." && sleep 1
 
 cd $HOME || return
-rm -rf nibiru
-git clone https://github.com/NibiruChain/nibiru
-cd $HOME/nibiru || return
-git checkout v0.19.2
+rm -rf lava
+git clone https://github.com/lavanet/lava
+cd $HOME/lava || return
+git checkout $BINARY_VERSION_TAG
 make install
-nibid version # v0.19.2
+lavad version # v0.7.0
 
-nibid config keyring-backend os
-nibid config chain-id $CHAIN_ID
-nibid init "$NODE_MONIKER" --chain-id $CHAIN_ID
+lavad config keyring-backend os
+lavad config chain-id $CHAIN_ID
+lavad init "$NODE_MONIKER" --chain-id $CHAIN_ID
 
-curl -s https://rpc.itn-1.nibiru.fi/genesis | jq -r .result.genesis > $HOME/.nibid/config/genesis.json
-curl -s https://snapshots2-testnet.nodejumper.io/nibiru-testnet/addrbook.json > $HOME/.nibid/config/addrbook.json
+curl -s https://raw.githubusercontent.com/K433QLtr6RA9ExEq/GHFkqmTzpdNLDd6T/main/testnet-1/genesis_json/genesis.json > $HOME/.lava/config/genesis.json
+curl -s https://snapshots1-testnet.nodejumper.io/lava-testnet/addrbook.json > $HOME/.lava/config/addrbook.json
 
-CONFIG_TOML=$HOME/.nibid/config/config.toml
+CONFIG_TOML=$HOME/.lava/config/config.toml
 PEERS=""
 sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $CONFIG_TOML
-SEEDS="3f472746f46493309650e5a033076689996c8881@nibiru-testnet.rpc.kjnodes.com:39659,a431d3d1b451629a21799963d9eb10d83e261d2c@seed-1.itn-1.nibiru.fi:26656,6a78a2a5f19c93661a493ecbe69afc72b5c54117@seed-2.itn-1.nibiru.fi:26656"
+SEEDS=SEEDS="3a445bfdbe2d0c8ee82461633aa3af31bc2b4dc0@prod-pnet-seed-node.lavanet.xyz:26656,e593c7a9ca61f5616119d6beb5bd8ef5dd28d62d@prod-pnet-seed-node2.lavanet.xyz:26656"
 sed -i.bak -e "s/^seeds =.*/seeds = \"$SEEDS\"/" $CONFIG_TOML
 
-APP_TOML=$HOME/.nibid/config/app.toml
+APP_TOML=$HOME/.lava/config/app.toml
 sed -i 's|^pruning *=.*|pruning = "custom"|g' $APP_TOML
 sed -i 's|^pruning-keep-recent  *=.*|pruning-keep-recent = "1000"|g' $APP_TOML
 sed -i 's|^pruning-interval *=.*|pruning-interval = "10"|g' $APP_TOML
@@ -61,17 +61,17 @@ sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.025unibi"|g' $APP_TOM
 sed -i.bak -e "s/^external_address *=.*/external_address = \"$(wget -qO- eth0.me):$PORT_PPROF_LADDR\"/" $CONFIG_TOML
 sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:$PORT_PROXY_APP\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:$PORT_RPC\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:$PORT_P2P\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:$PORT_PPROF_LADDR\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":$PORT_PROMETHEUS\"%" $CONFIG_TOML && \
 sed -i.bak -e "s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:$PORT_GRPC\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:$PORT_GRPC_WEB\"%; s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:$PORT_API\"%" $APP_TOML && \
-sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:$PORT_RPC\"%" $HOME/.nibid/config/client.toml
+sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:$PORT_RPC\"%" $HOME/.lava/config/client.toml
 
 echo "Starting service and synchronization..." && sleep 1
 
-sudo tee /etc/systemd/system/nibid.service > /dev/null << EOF
+sudo tee /etc/systemd/system/lavad.service > /dev/null << EOF
 [Unit]
-Description=Nibiru Node
+Description=Lava Node
 After=network-online.target
 [Service]
 User=$USER
-ExecStart=$(which nibid) start
+ExecStart=$(which lavad) start
 Restart=on-failure
 RestartSec=10
 LimitNOFILE=10000
@@ -79,15 +79,15 @@ LimitNOFILE=10000
 WantedBy=multi-user.target
 EOF
 
-nibid tendermint unsafe-reset-all --home $HOME/.nibid --keep-addr-book
+lavad tendermint unsafe-reset-all --home $HOME/.lava --keep-addr-book
 
 # Add snapshot here
-SNAP_NAME=$(curl -s https://snapshots2-testnet.nodejumper.io/nibiru-testnet/info.json | jq -r .fileName)
-curl "https://snapshots2-testnet.nodejumper.io/nibiru-testnet/${SNAP_NAME}" | lz4 -dc - | tar -xf - -C $HOME/.nibid
+SNAP_NAME=$(curl -s https://snapshots1-testnet.nodejumper.io/lava-testnet/info.json | jq -r .fileName)
+curl "https://snapshots1-testnet.nodejumper.io/lava-testnet/${SNAP_NAME}" | lz4 -dc - | tar -xf - -C "$HOME/.lava"
 
 sudo systemctl daemon-reload
-sudo systemctl enable nibid
-sudo systemctl start nibid
+sudo systemctl enable lavad
+sudo systemctl start lavad
 
 printDelimiter
 echo -e "Check logs:            sudo journalctl -u $BINARY_NAME -f -o cat"
