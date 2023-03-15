181c0f2766287508a342caf38dc81ff1741aa85e
mogoll92 <mogoll92@gmail.com>
2023-03-15 22:06:38 +0200

    Added source node

---
+++ source/install.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+source <(curl -s https://raw.githubusercontent.com/R1M-NODES/utils/master/common.sh)
+
+printLogo
+
+source <(curl -s https://raw.githubusercontent.com/R1M-NODES/cosmos/master/utils/ports.sh) && sleep 1
+export -f selectPortSet && selectPortSet
+
+read -r -p "Enter node moniker: " NODE_MONIKER
+
+CHAIN_ID="sourcechain-testnet"
+CHAIN_DENOM="usource"
+BINARY_NAME="sourced"
+BINARY_VERSION_TAG="e06b810e842e57ec8f5432c9cdd57883a69b3cee"
+CHEAT_SHEET=""
+
+printDelimiter
+echo -e "Node moniker:       $NODE_MONIKER"
+echo -e "Chain id:           $CHAIN_ID"
+echo -e "Chain demon:        $CHAIN_DENOM"
+echo -e "Binary version tag: $BINARY_VERSION_TAG"
+printDelimiter && sleep 1
+
+source <(curl -s https://raw.githubusercontent.com/R1M-NODES/cosmos/master/utils/dependencies.sh)
+
+echo "" && printGreen "Building binaries..." && sleep 1
+
+cd $HOME || return
+rm -rf source
+git clone https://github.com/Source-Protocol-Cosmos/source.git
+cd $HOME/source || return
+git checkout e06b810e842e57ec8f5432c9cdd57883a69b3cee
+make install
+sourced version # e06b810e842e57ec8f5432c9cdd57883a69b3cee
+
+sourced config keyring-backend os
+sourced config chain-id $CHAIN_ID
+sourced init "$NODE_MONIKER" --chain-id $CHAIN_ID
+
+curl -Ls https://snapshots.kjnodes.com/source-testnet/genesis.json > $HOME/.source/config/genesis.json
+curl -Ls https://snapshots.kjnodes.com/source-testnet/addrbook.json > $HOME/.source/config/addrbook.json
+
+CONFIG_TOML=$HOME/.source/config/config.toml
+PEERS=""
+sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $CONFIG_TOML
+SEEDS="3f472746f46493309650e5a033076689996c8881@source-testnet.rpc.kjnodes.com:28659"
+sed -i.bak -e "s/^seeds =.*/seeds = \"$SEEDS\"/" $CONFIG_TOML
+
+APP_TOML=$HOME/.source/config/app.toml
+sed -i 's|^pruning *=.*|pruning = "custom"|g' $APP_TOML
+sed -i 's|^pruning-keep-recent  *=.*|pruning-keep-recent = "1000"|g' $APP_TOML
+sed -i 's|^pruning-interval *=.*|pruning-interval = "10"|g' $APP_TOML
+sed -i -e "s/^filter_peers *=.*/filter_peers = \"true\"/" $CONFIG_TOML
+indexer="null"
+sed -i -e "s/^indexer *=.*/indexer = \"$indexer\"/" $CONFIG_TOML
+sed -i 's|^snapshot-interval *=.*|snapshot-interval = 1000|g' $APP_TOML
+sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.025unibi"|g' $APP_TOML
+
+# Customize ports
+CLIENT_TOML=$HOME/.source/config/client.toml
+sed -i.bak -e "s/^external_address *=.*/external_address = \"$(wget -qO- eth0.me):$PORT_PPROF_LADDR\"/" $CONFIG_TOML
+sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:$PORT_PROXY_APP\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:$PORT_RPC\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:$PORT_P2P\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:$PORT_PPROF_LADDR\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":$PORT_PROMETHEUS\"%" $CONFIG_TOML && \
+sed -i.bak -e "s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:$PORT_GRPC\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:$PORT_GRPC_WEB\"%; s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:$PORT_API\"%" $APP_TOML && \
+sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:$PORT_RPC\"%" $CLIENT_TOML
+
+printGreen "Starting service and synchronization..." && sleep 1
+
+sudo tee /etc/systemd/system/sourced.service > /dev/null << EOF
+[Unit]
+Description=Source Node
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=$(which sourced) start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=10000
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sourced tendermint unsafe-reset-all --home $HOME/.source --keep-addr-book
+
+# Add snapshot here
+curl -L https://snapshots.kjnodes.com/source-testnet/snapshot_latest.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.source
+
+sudo systemctl daemon-reload
+sudo systemctl enable sourced
+sudo systemctl start sourced
+
+printDelimiter
+printGreen "Check logs:            sudo journalctl -u $BINARY_NAME -f -o cat"
+printGreen "Check synchronization: $BINARY_NAME status 2>&1 | jq .SyncInfo.catching_up"
+printGreen "Check our cheat sheet: $CHEAT_SHEET"
+printDelimiter
