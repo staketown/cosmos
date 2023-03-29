aa98bb33440e2daf0294172cd1022a11ec888b5f
mogoll92 <mogoll92@gmail.com>
2023-03-29 16:47:04 +0300

    Install nolus

---
+++ nolus/install.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+source <(curl -s https://raw.githubusercontent.com/R1M-NODES/utils/master/common.sh)
+
+printLogo
+
+source <(curl -s https://raw.githubusercontent.com/R1M-NODES/cosmos/master/utils/ports.sh) && sleep 1
+export -f selectPortSet && selectPortSet
+
+read -r -p "Enter node moniker: " NODE_MONIKER
+
+CHAIN_ID="nolus-rila"
+CHAIN_DENOM="unls"
+BINARY_NAME="nolud"
+BINARY_VERSION_TAG="v0.2.1-testnet"
+CHEAT_SHEET="https://nodes.r1m-team.com/nolus"
+
+printDelimiter
+echo -e "Node moniker:       $NODE_MONIKER"
+echo -e "Chain id:           $CHAIN_ID"
+echo -e "Chain demon:        $CHAIN_DENOM"
+echo -e "Binary version tag: $BINARY_VERSION_TAG"
+printDelimiter && sleep 1
+
+source <(curl -s https://raw.githubusercontent.com/R1M-NODES/cosmos/master/utils/dependencies.sh)
+
+echo "" && printGreen "Building binaries..." && sleep 1
+
+cd $HOME || return
+rm -rf nolus-core
+git clone https://github.com/Nolus-Protocol/nolus-core.git
+cd nolus-core
+git checkout $BINARY_VERSION_TAG
+
+nolud config keyring-backend os
+nolud config chain-id $CHAIN_ID
+nolud init "$NODE_MONIKER" --chain-id $CHAIN_ID
+
+curl -Ls https://snapshots.kjnodes.com/nolus-testnet/genesis.json > $HOME/.nolus/config/genesis.json
+curl -Ls https://snapshots.kjnodes.com/nolus-testnet/addrbook.json > $HOME/.nolus/config/addrbook.json
+
+#curl -s https://snapshots-testnet.r1m-team.com/nibiru/genesis.json > $HOME/.nibid/config/genesis.json
+#curl -s https://snapshots-testnet.r1m-team.com/nibiru/addrbook.json > $HOME/.nibid/config/addrbook.json
+
+CONFIG_TOML=$HOME/.nolus/config/config.toml
+PEERS=""
+sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $CONFIG_TOML
+SEEDS="3f472746f46493309650e5a033076689996c8881@nolus-testnet.rpc.kjnodes.com:43659"
+sed -i.bak -e "s/^seeds =.*/seeds = \"$SEEDS\"/" $CONFIG_TOML
+
+APP_TOML=$HOME/.nolus/config/app.toml
+sed -i 's|^pruning *=.*|pruning = "custom"|g' $APP_TOML
+sed -i 's|^pruning-keep-recent  *=.*|pruning-keep-recent = "100"|g' $APP_TOML
+sed -i 's|^pruning-keep-every *=.*|pruning-keep-every = "0"|g' $APP_TOML
+sed -i 's|^pruning-interval *=.*|pruning-interval = "10"|g' $APP_TOML
+sed -i -e "s/^filter_peers *=.*/filter_peers = \"true\"/" $CONFIG_TOML
+indexer="null"
+sed -i -e "s/^indexer *=.*/indexer = \"$indexer\"/" $CONFIG_TOML
+sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.025unls"|g' $APP_TOML
+
+# Customize ports
+CLIENT_TOML=$HOME/.nolus/config/client.toml
+sed -i.bak -e "s/^external_address *=.*/external_address = \"$(wget -qO- eth0.me):$PORT_PPROF_LADDR\"/" $CONFIG_TOML
+sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:$PORT_PROXY_APP\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:$PORT_RPC\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:$PORT_P2P\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:$PORT_PPROF_LADDR\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":$PORT_PROMETHEUS\"%" $CONFIG_TOML && \
+sed -i.bak -e "s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:$PORT_GRPC\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:$PORT_GRPC_WEB\"%; s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:$PORT_API\"%" $APP_TOML && \
+sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:$PORT_RPC\"%" $CLIENT_TOML
+
+printGreen "Starting service and synchronization..." && sleep 1
+
+sudo tee /etc/systemd/system/nolusd.service > /dev/null << EOF
+[Unit]
+Description=Nolus Node
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=$(which nolud) start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=10000
+[Install]
+WantedBy=multi-user.target
+EOF
+
+nolud tendermint unsafe-reset-all --home $HOME/.nolus --keep-addr-book
+
+# Add snapshot here
+curl -L https://snapshots.kjnodes.com/nolus-testnet/snapshot_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.nolus
+# URL="https://snapshots-testnet.r1m-team.com/nibiru/nibiru-itn-1_latest.tar.lz4"
+# curl $URL | lz4 -dc - | tar -xf - -C $HOME/.nibid
+
+sudo systemctl daemon-reload
+sudo systemctl enable nolusd
+sudo systemctl start nolusd
+
+printDelimiter
+printGreen "Check logs:            sudo journalctl -u nolusd -f -o cat"
+printGreen "Check synchronization: $BINARY_NAME status 2>&1 | jq .SyncInfo.catching_up"
+printGreen "Check our cheat sheet: $CHEAT_SHEET"
+printDelimiter
