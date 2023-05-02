74c5905ec11668e49c5e5f5ca6bf94eae76abe4a
mogoll92 <mogoll92@gmail.com>
2023-05-02 18:48:01 +0300

    Updated ports

--- utils/ports.sh
+++ utils/ports.sh
@@ -72,7 +72,7 @@ function selectPortSet {
     7) exportPorts 7 ;;
     8) exportPorts 8 ;;
     9) exportPorts 9 ;;
-    -?[0-9]+) exportPorts ${flag} ;;
+    [1-9][0-9]*) exportPorts ${flag} ;;
     *) printGreen "WARN: unknown parameter: ${flag}" && exit 1 ;;
   esac
 }
