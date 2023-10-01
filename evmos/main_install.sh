4fdaba438017983eda4edcf0703427427b418b57
Yevhen Sydorenko <mogoll92@gmail.com>
2023-10-01 11:54:53 +0300

    Updated evmos

--- evmos/main_install.sh
+++ evmos/main_install.sh
@@ -56,7 +56,7 @@ sed -i 's|^pruning-interval *=.*|pruning-interval = "19"|g' $APP_TOML
 sed -i -e "s/^filter_peers *=.*/filter_peers = \"true\"/" $CONFIG_TOML
 indexer="null"
 sed -i -e "s/^indexer *=.*/indexer = \"$indexer\"/" $CONFIG_TOML
-sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.001aevmos"|g' $APP_TOML
+sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "250000000aevmos"|g' $APP_TOML
 
 # Customize ports
 CLIENT_TOML=$HOME/.evmosd/config/client.toml
