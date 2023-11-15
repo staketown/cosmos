63fab39c978308f5e81cf61d9fcd153793a3176a
Yevhen Sydorenko <mogoll92@gmail.com>
2023-11-15 16:52:50 +0200

    Updated umee oracle config

--- umee/price_feeder_install.sh
+++ umee/price_feeder_install.sh
@@ -25,11 +25,11 @@ make build
 sudo mv ./build/price-feeder /usr/local/bin/umee-price-feeder
 rm $HOME/.umee-price-feeder -rf
 mkdir $HOME/.umee-price-feeder
-curl -s https://raw.githubusercontent.com/ojo-network/price-feeder/umee/umee-provider-config/currency-pairs.toml > $HOME/.umee-price-feeder/config.toml
+curl -s https://raw.githubusercontent.com/ojo-network/price-feeder/umee/v2.1.8/umee-provider-configcurrency-pairs.toml > $HOME/.umee-price-feeder/currency-pairs.toml
 echo >> $HOME/.umee-price-feeder/config.toml
-curl -s https://raw.githubusercontent.com/ojo-network/price-feeder/umee/umee-provider-config/deviation-thresholds.toml >> $HOME/.umee-price-feeder/config.toml
+curl -s https://raw.githubusercontent.com/ojo-network/price-feeder/umee/v2.1.8/umee-provider-config/deviation-thresholds.toml >> $HOME/.umee-price-feeder/deviation-thresholds.toml
 echo >> $HOME/.umee-price-feeder/config.toml
-curl -s https://raw.githubusercontent.com/ojo-network/price-feeder/umee/umee-provider-config/endpoints.toml >> $HOME/.umee-price-feeder/config.toml
+curl -s https://raw.githubusercontent.com/ojo-network/price-feeder/umee/v2.1.8/umee-provider-config/endpoints.toml >> $HOME/.umee-price-feeder/endpoints.toml
 echo >> $HOME/.umee-price-feeder/config.toml
 curl -s https://raw.githubusercontent.com/ojo-network/price-feeder/umee/price-feeder.example.toml >> $HOME/.umee-price-feeder/config.toml
 
