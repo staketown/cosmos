10f19fab584e058cc5c5349e83f64ee43832914b
Yevhen Sydorenko <mogoll92@gmail.com>
2023-12-06 13:09:45 +0200

    Updated juno

--- juno/main_install.sh
+++ juno/main_install.sh
@@ -12,7 +12,7 @@ read -r -p "Enter node moniker: " NODE_MONIKER
 CHAIN_ID="juno-1"
 CHAIN_DENOM="ujuno"
 BINARY_NAME="junod"
-BINARY_VERSION_TAG="v17.0.0"
+BINARY_VERSION_TAG="v18.0.0"
 CHEAT_SHEET=""
 
 printDelimiter
