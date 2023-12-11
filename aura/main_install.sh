98fe8caeacd3481f12af8d246468630de4890d5e
Yevhen Sydorenko <mogoll92@gmail.com>
2023-12-11 13:07:09 +0200

    Updated aura version

--- aura/main_install.sh
+++ aura/main_install.sh
@@ -12,7 +12,7 @@ read -r -p "Enter node moniker: " NODE_MONIKER
 CHAIN_ID="xstaxy-1"
 CHAIN_DENOM="uaura"
 BINARY_NAME="aurad"
-BINARY_VERSION_TAG="v0.7.1"
+BINARY_VERSION_TAG="v0.7.2"
 CHEAT_SHEET=""
 
 printDelimiter
