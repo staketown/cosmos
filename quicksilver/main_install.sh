f903b73dbcb8e0ccc3f747606be302fb1acfb457
Yevhen Sydorenko <mogoll92@gmail.com>
2024-01-06 13:37:36 +0200

    Updated quicksilver

--- quicksilver/main_install.sh
+++ quicksilver/main_install.sh
@@ -12,7 +12,7 @@ read -r -p "Enter node moniker: " NODE_MONIKER
 CHAIN_ID="quicksilver-2"
 CHAIN_DENOM="uqck"
 BINARY_NAME="umeed"
-BINARY_VERSION_TAG="v1.2.17"
+BINARY_VERSION_TAG="v1.4.5"
 CHEAT_SHEET=""
 
 printDelimiter
