6a4736554c2d8e944206d55d9139a6562c25c457
Yevhen Sydorenko <mogoll92@gmail.com>
2024-01-10 16:27:28 +0200

    Updated juno and quicksilver

--- quicksilver/main_install.sh
+++ quicksilver/main_install.sh
@@ -12,7 +12,7 @@ read -r -p "Enter node moniker: " NODE_MONIKER
 CHAIN_ID="quicksilver-2"
 CHAIN_DENOM="uqck"
 BINARY_NAME="umeed"
-BINARY_VERSION_TAG="v1.4.5"
+BINARY_VERSION_TAG="v1.4.6"
 CHEAT_SHEET=""
 
 printDelimiter
