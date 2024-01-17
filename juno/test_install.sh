289fc554febcefa7b5a21c4b752bcea768233b5b
Yevhen Sydorenko <mogoll92@gmail.com>
2024-01-17 17:36:53 +0200

    Updated testnet

--- juno/test_install.sh
+++ juno/test_install.sh
@@ -12,7 +12,7 @@ read -r -p "Enter node moniker: " NODE_MONIKER
 CHAIN_ID="uni-6"
 CHAIN_DENOM="ujunox"
 BINARY_NAME="junod"
-BINARY_VERSION_TAG="v18.1.0"
+BINARY_VERSION_TAG="v19.0.0-alpha.1"
 CHEAT_SHEET=""
 
 printDelimiter
