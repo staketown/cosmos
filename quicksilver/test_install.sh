ebe426bfd4f0dea71b0f082b144d8bef7e773714
Yevhen Sydorenko <mogoll92@gmail.com>
2024-02-01 19:59:27 +0200

    quick silver

--- quicksilver/test_install.sh
+++ quicksilver/test_install.sh
@@ -12,7 +12,7 @@ read -r -p "Enter node moniker: " NODE_MONIKER
 CHAIN_ID="rhye-2"
 CHAIN_DENOM="uqck"
 BINARY_NAME="umeed"
-BINARY_VERSION_TAG="v1.4.7-rc1"
+BINARY_VERSION_TAG="v1.4.7-rc2"
 CHEAT_SHEET=""
 
 printDelimiter
