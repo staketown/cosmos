ffc2c873ea6bf95be5080dd40e90faedca547478
Yevhen Sydorenko <mogoll92@gmail.com>
2024-02-12 21:09:30 +0200

    Updated celestia

--- cascadia/test_install.sh
+++ cascadia/test_install.sh
@@ -9,10 +9,10 @@ export -f selectPortSet && selectPortSet
 
 read -r -p "Enter node moniker: " NODE_MONIKER
 
-CHAIN_ID="cascadia_11029-1"
-CHAIN_DENOM="aCC"
-BINARY_NAME="cascadiad"
-BINARY_VERSION_TAG="v0.3.0"
+CHAIN_ID="mocha-4"
+CHAIN_DENOM="utia"
+BINARY_NAME="celestia-appd"
+BINARY_VERSION_TAG="v1.3.0"
 CHEAT_SHEET=""
 
 printDelimiter
@@ -26,40 +26,41 @@ source <(curl -s https://raw.githubusercontent.com/staketown/cosmos/master/utils
 
 echo "" && printGreen "Building binaries..." && sleep 1
 
-
 cd $HOME || return
-rm -rf cascadia
-git clone https://github.com/cascadiafoundation/cascadia
-cd $HOME/cascadia || return
+rm -rf celestia-app
+git clone https://github.com/celestiaorg/celestia-app.git
+cd $HOME/celestia-app || return
 git checkout $BINARY_VERSION_TAG
 
 make install
 
-cascadiad config keyring-backend os
-cascadiad config chain-id $CHAIN_ID
-cascadiad init "$NODE_MONIKER" --chain-id $CHAIN_ID
+celestia-appd config keyring-backend os
+celestia-appd config chain-id $CHAIN_ID
+celestia-appd init "$NODE_MONIKER" --chain-id $CHAIN_ID
 
-curl -s https://snapshots-testnet.stake-town.com/cascadia/genesis.json > $HOME/.cascadiad/config/genesis.json
-curl -s https://snapshots-testnet.stake-town.com/cascadia/addrbook.json > $HOME/.cascadiad/config/addrbook.json
+curl -Ls https://snapshots.kjnodes.com/celestia-testnet/genesis.json > $HOME/.celestia-app/config/genesis.json
+curl -Ls https://snapshots.kjnodes.com/celestia-testnet/addrbook.json > $HOME/.celestia-app/config/addrbook.json
+#curl -s https://snapshots-testnet.stake-town.com/cascadia/genesis.json > $HOME/.cascadiad/config/genesis.json
+#curl -s https://snapshots-testnet.stake-town.com/cascadia/addrbook.json > $HOME/.cascadiad/config/addrbook.json
 
-CONFIG_TOML=$HOME/.cascadiad/config/config.toml
-PEERS="0c96a6c328eb58d1467afff4130ab446c294108c@34.239.67.55:26656,d1ed80e232fc2f3742637daacab454e345bbe475@54.204.246.120:26656"
+CONFIG_TOML=$HOME/.celestia-app/config/config.toml
+PEERS=""
 sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $CONFIG_TOML
-SEEDS=""
+SEEDS="3f472746f46493309650e5a033076689996c8881@celestia-testnet.rpc.kjnodes.com:12059"
 sed -i.bak -e "s/^seeds =.*/seeds = \"$SEEDS\"/" $CONFIG_TOML
 
-APP_TOML=$HOME/.cascadiad/config/app.toml
-sed -i 's|^pruning *=.*|pruning = "custom"|g' $APP_TOML
-sed -i 's|^pruning-keep-recent  *=.*|pruning-keep-recent = "100"|g' $APP_TOML
-sed -i 's|^pruning-keep-every *=.*|pruning-keep-every = "0"|g' $APP_TOML
-sed -i 's|^pruning-interval *=.*|pruning-interval = "19"|g' $APP_TOML
+APP_TOML=$HOME/.celestia-app/config/app.toml
+sed -i 's|^pruning *=.*|pruning = "nothing"|g' $APP_TOML
+#sed -i 's|^pruning-keep-recent  *=.*|pruning-keep-recent = "100"|g' $APP_TOML
+#sed -i 's|^pruning-keep-every *=.*|pruning-keep-every = "0"|g' $APP_TOML
+#sed -i 's|^pruning-interval *=.*|pruning-interval = "19"|g' $APP_TOML
 sed -i -e "s/^filter_peers *=.*/filter_peers = \"true\"/" $CONFIG_TOML
 indexer="null"
-sed -i -e "s/^indexer *=.*/indexer = \"$indexer\"/" $CONFIG_TOML
-sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.0025aCC"|g' $APP_TOML
+#sed -i -e "s/^indexer *=.*/indexer = \"$indexer\"/" $CONFIG_TOML
+sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.002utia"|g' $APP_TOML
 
 # Customize ports
-CLIENT_TOML=$HOME/.cascadiad/config/client.toml
+CLIENT_TOML=$HOME/.celestia-app/config/client.toml
 sed -i.bak -e "s/^external_address *=.*/external_address = \"$(wget -qO- eth0.me):$PORT_PPROF_LADDR\"/" $CONFIG_TOML
 sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:$PORT_PROXY_APP\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:$PORT_RPC\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:$PORT_P2P\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:$PORT_PPROF_LADDR\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":$PORT_PROMETHEUS\"%" $CONFIG_TOML && \
 sed -i.bak -e "s%^address = \"localhost:9090\"%address = \"0.0.0.0:$PORT_GRPC\"%; s%^address = \"localhost:9091\"%address = \"0.0.0.0:$PORT_GRPC_WEB\"%; s%^address = \"tcp://localhost:1317\"%address = \"tcp://0.0.0.0:$PORT_API\"%" $APP_TOML && \
@@ -68,15 +69,15 @@ sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:$POR
 printGreen "Install and configure cosmovisor..." && sleep 1
 
 go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.4.0
-mkdir -p ~/.cascadiad/cosmovisor/genesis/bin
-mkdir -p ~/.cascadiad/cosmovisor/upgrades
-cp ~/go/bin/cascadiad $HOME/.cascadiad/cosmovisor/genesis/bin
+mkdir -p ~/.celestia-app/cosmovisor/genesis/bin
+mkdir -p ~/.celestia-app/cosmovisor/upgrades
+cp ~/go/bin/celestia-appd $HOME/.celestia-app/cosmovisor/genesis/bin
 
 printGreen "Starting service and synchronization..." && sleep 1
 
-sudo tee /etc/systemd/system/cascadiad.service > /dev/null << EOF
+sudo tee /etc/systemd/system/celestia-appd.service > /dev/null << EOF
 [Unit]
-Description=Cascadia Node
+Description=Celestia Node
 After=network-online.target
 [Service]
 User=$USER
@@ -84,8 +85,8 @@ ExecStart=$(which cosmovisor) run start --chain-id $CHAIN_ID
 Restart=on-failure
 RestartSec=3
 LimitNOFILE=10000
-Environment="DAEMON_NAME=cascadiad"
-Environment="DAEMON_HOME=$HOME/.cascadiad"
+Environment="DAEMON_NAME=celestia-appd"
+Environment="DAEMON_HOME=$HOME/.celestia-app"
 Environment="DAEMON_ALLOW_DOWNLOAD_BINARIES=false"
 Environment="DAEMON_RESTART_AFTER_UPGRADE=true"
 Environment="UNSAFE_SKIP_BACKUP=true"
@@ -93,16 +94,16 @@ Environment="UNSAFE_SKIP_BACKUP=true"
 WantedBy=multi-user.target
 EOF
 
-cascadiad tendermint unsafe-reset-all --home $HOME/.cascadiad --keep-addr-book
+celestia-appd tendermint unsafe-reset-all --home $HOME/.celestia-app --keep-addr-book
 
 # Add snapshot here
-URL="https://snapshots-testnet.stake-town.com/cascadia/cascadia_11029-1_latest.tar.lz4"
-curl $URL | lz4 -dc - | tar -xf - -C $HOME/.cascadiad
-[[ -f $HOME/.cascadiad/data/upgrade-info.json ]] && cp $HOME/.cascadiad/data/upgrade-info.json $HOME/.cascadiad/cosmovisor/genesis/upgrade-info.json
+URL="https://snapshots.kjnodes.com/celestia-testnet/snapshot_latest.tar.lz4"
+curl $URL | lz4 -dc - | tar -xf - -C $HOME/.celestia-app
+[[ -f $HOME/.celestia-app/data/upgrade-info.json ]] && cp $HOME/.celestia-app/data/upgrade-info.json $HOME/.celestia-app/cosmovisor/genesis/upgrade-info.json
 
 sudo systemctl daemon-reload
-sudo systemctl enable cascadiad
-sudo systemctl start cascadiad
+sudo systemctl enable celestia-appd
+sudo systemctl start celestia-appd
 
 printDelimiter
 printGreen "Check logs:            sudo journalctl -u $BINARY_NAME -f -o cat"
