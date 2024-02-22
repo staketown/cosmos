5e4e04b5804db0540ee025a88e81681bd3cf0d79
Yevhen Sydorenko <mogoll92@gmail.com>
2024-02-22 08:17:24 +0200

    Added Planq node

--- planq/main_install.sh
+++ planq/main_install.sh
@@ -9,10 +9,10 @@ export -f selectPortSet && selectPortSet
 
 read -r -p "Enter node moniker: " NODE_MONIKER
 
-CHAIN_ID="euphoria-2"
-CHAIN_DENOM="ueaura"
-BINARY_NAME="aurad"
-BINARY_VERSION_TAG="euphoria_v0.7.0"
+CHAIN_ID="planq_7070-2"
+CHAIN_DENOM="aplanq"
+BINARY_NAME="planqd"
+BINARY_VERSION_TAG="v1.1.0"
 CHEAT_SHEET=""
 
 printDelimiter
@@ -27,27 +27,28 @@ source <(curl -s https://raw.githubusercontent.com/staketown/cosmos/master/utils
 echo "" && printGreen "Building binaries..." && sleep 1
 
 cd $HOME || return
-rm -rf aura
-git clone https://github.com/aura-nw/aura.git
-cd aura || return
+rm -rf planq
+git clone https://github.com/planq-network/planq.git
+cd planq || return
 git checkout $BINARY_VERSION_TAG
 
 make install
 
-aurad config keyring-backend os
-aurad config chain-id $CHAIN_ID
-aurad init "$NODE_MONIKER" --chain-id $CHAIN_ID
+planqd config keyring-backend os
+planqd config chain-id $CHAIN_ID
+planqd init "$NODE_MONIKER" --chain-id $CHAIN_ID
 
-curl -s https://snapshots-testnet.stake-town.com/aura/genesis.json > $HOME/.aura/config/genesis.json
-curl -s https://snapshots-testnet.stake-town.com/aura/addrbook.json > $HOME/.aura/config/addrbook.json
+curl -s https://snapshots.polkachu.com/genesis/planq/genesis.json > $HOME/.planqd/config/genesis.json
+#curl -s https://snapshots.stake-town.com/planq/genesis.json > $HOME/.planqd/config/genesis.json
+#curl -s https://snapshots.stake-town.com/planq/addrbook.json > $HOME/.planqd/config/addrbook.json
 
-CONFIG_TOML=$HOME/.aura/config/config.toml
+CONFIG_TOML=$HOME/.planqd/config/config.toml
 PEERS=""
 sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $CONFIG_TOML
-SEEDS="705e3c2b2b554586976ed88bb27f68e4c4176a33@52.76.203.126:26656"
+SEEDS="ade4d8bc8cbe014af6ebdf3cb7b1e9ad36f412c0@seeds.polkachu.com:20356"
 sed -i.bak -e "s/^seeds =.*/seeds = \"$SEEDS\"/" $CONFIG_TOML
 
-APP_TOML=$HOME/.aura/config/app.toml
+APP_TOML=$HOME/.planqd/config/app.toml
 sed -i 's|^pruning *=.*|pruning = "custom"|g' $APP_TOML
 sed -i 's|^pruning-keep-recent  *=.*|pruning-keep-recent = "100"|g' $APP_TOML
 sed -i 's|^pruning-keep-every *=.*|pruning-keep-every = "0"|g' $APP_TOML
@@ -55,10 +56,10 @@ sed -i 's|^pruning-interval *=.*|pruning-interval = "19"|g' $APP_TOML
 sed -i -e "s/^filter_peers *=.*/filter_peers = \"true\"/" $CONFIG_TOML
 indexer="null"
 sed -i -e "s/^indexer *=.*/indexer = \"$indexer\"/" $CONFIG_TOML
-sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0ueaura"|g' $APP_TOML
+sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0aplanq"|g' $APP_TOML
 
 # Customize ports
-CLIENT_TOML=$HOME/.aura/config/client.toml
+CLIENT_TOML=$HOME/.planqd/config/client.toml
 sed -i.bak -e "s/^external_address *=.*/external_address = \"$(wget -qO- eth0.me):$PORT_PPROF_LADDR\"/" $CONFIG_TOML
 sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:$PORT_PROXY_APP\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:$PORT_RPC\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:$PORT_P2P\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:$PORT_PPROF_LADDR\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":$PORT_PROMETHEUS\"%" $CONFIG_TOML && \
 sed -i.bak -e "s%^address = \"localhost:9090\"%address = \"0.0.0.0:$PORT_GRPC\"%; s%^address = \"localhost:9091\"%address = \"0.0.0.0:$PORT_GRPC_WEB\"%; s%^address = \"tcp://localhost:1317\"%address = \"tcp://0.0.0.0:$PORT_API\"%" $APP_TOML && \
@@ -67,15 +68,15 @@ sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:$POR
 printGreen "Install and configure cosmovisor..." && sleep 1
 
 go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.4.0
-mkdir -p ~/.aura/cosmovisor/genesis/bin
-mkdir -p ~/.aura/cosmovisor/upgrades
-cp ~/go/bin/aurad ~/.aura/cosmovisor/genesis/bin
+mkdir -p ~/.planqd/cosmovisor/genesis/bin
+mkdir -p ~/.planqd/cosmovisor/upgrades
+cp ~/go/bin/planqd ~/.planqd/cosmovisor/genesis/bin
 
 printGreen "Starting service and synchronization..." && sleep 1
 
-sudo tee /etc/systemd/system/aurad.service > /dev/null << EOF
+sudo tee /etc/systemd/system/planqd.service > /dev/null << EOF
 [Unit]
-Description=Aura Node
+Description=Planq Node
 After=network-online.target
 [Service]
 User=$USER
@@ -83,8 +84,8 @@ ExecStart=$(which cosmovisor) run start
 Restart=on-failure
 RestartSec=3
 LimitNOFILE=10000
-Environment="DAEMON_NAME=aurad"
-Environment="DAEMON_HOME=$HOME/.aura"
+Environment="DAEMON_NAME=planqd"
+Environment="DAEMON_HOME=$HOME/.planqd"
 Environment="DAEMON_ALLOW_DOWNLOAD_BINARIES=false"
 Environment="DAEMON_RESTART_AFTER_UPGRADE=true"
 Environment="UNSAFE_SKIP_BACKUP=true"
@@ -92,16 +93,17 @@ Environment="UNSAFE_SKIP_BACKUP=true"
 WantedBy=multi-user.target
 EOF
 
-aurad tendermint unsafe-reset-all --home $HOME/.aura --keep-addr-book
+planqd tendermint unsafe-reset-all --home $HOME/.planqd --keep-addr-book
 
 # Add snapshot here
-URL="https://snapshots-testnet.stake-town.com/aura/euphoria-2_latest.tar.lz4"
-curl -L $URL | lz4 -dc - | tar -xf - -C $HOME/.aura
-[[ -f $HOME/.aura/data/upgrade-info.json ]]  && cp $HOME/.aura/data/upgrade-info.json $HOME/.aura/cosmovisor/genesis/upgrade-info.json
+URL="https://snapshots.polkachu.com/snapshots/planq/planq_7588883.tar.lz4"
+#URL="https://snapshots.stake-town.com/aura/xstaxy-1_latest.tar.lz4"
+curl -L $URL | lz4 -dc - | tar -xf - -C $HOME/.planqd
+[[ -f $HOME/.planqd/data/upgrade-info.json ]]  && cp $HOME/.planqd/data/upgrade-info.json $HOME/.planqd/cosmovisor/genesis/upgrade-info.json
 
 sudo systemctl daemon-reload
-sudo systemctl enable aurad
-sudo systemctl start aurad
+sudo systemctl enable planqd
+sudo systemctl start planqd
 
 printDelimiter
 printGreen "Check logs:            sudo journalctl -u $BINARY_NAME -f -o cat"
