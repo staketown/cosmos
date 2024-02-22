a1d7b56772ea6fad898ae50c60dccb658f4c470b
Yevhen Sydorenko <mogoll92@gmail.com>
2024-02-22 14:56:31 +0200

    Updated side

--- side/test_install.sh
+++ side/test_install.sh
@@ -9,10 +9,10 @@ export -f selectPortSet && selectPortSet
 
 read -r -p "Enter node moniker: " NODE_MONIKER
 
-CHAIN_ID="side-testnet-1"
+CHAIN_ID="side-testnet-2"
 CHAIN_DENOM="uside"
 BINARY_NAME="sided"
-BINARY_VERSION_TAG="0.0.1-75-gbd63479"
+BINARY_VERSION_TAG="v0.6.0"
 CHEAT_SHEET=""
 
 printDelimiter
@@ -38,16 +38,16 @@ sided config keyring-backend os
 sided config chain-id $CHAIN_ID
 sided init "$NODE_MONIKER" --chain-id $CHAIN_ID
 
-curl -Ls https://snapshots-testnet.stake-town.com/side/genesis.json > $HOME/.sidechain/config/genesis.json
-curl -Ls https://snapshots-testnet.stake-town.com/side/addrbook.json > $HOME/.sidechain/config/addrbook.json
+curl -Ls https://raw.githubusercontent.com/sideprotocol/testnet/main/side-testnet-2/genesis.json > $HOME/.side/config/genesis.json
+#curl -Ls https://snapshots-testnet.stake-town.com/side/addrbook.json > $HOME/.sidechain/config/addrbook.json
 
-CONFIG_TOML=$HOME/.sidechain/config/config.toml
+CONFIG_TOML=$HOME/.side/config/config.toml
 PEERS=""
 sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $CONFIG_TOML
 SEEDS="ade4d8bc8cbe014af6ebdf3cb7b1e9ad36f412c0@testnet-seeds.polkachu.com:26356"
 sed -i.bak -e "s/^seeds =.*/seeds = \"$SEEDS\"/" $CONFIG_TOML
 
-APP_TOML=$HOME/.sidechain/config/app.toml
+APP_TOML=$HOME/.side/config/app.toml
 sed -i 's|^pruning *=.*|pruning = "custom"|g' $APP_TOML
 sed -i 's|^pruning-keep-recent  *=.*|pruning-keep-recent = "100"|g' $APP_TOML
 sed -i 's|^pruning-keep-every *=.*|pruning-keep-every = "0"|g' $APP_TOML
@@ -58,7 +58,7 @@ sed -i -e "s/^indexer *=.*/indexer = \"$indexer\"/" $CONFIG_TOML
 sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.005uside"|g' $APP_TOML
 
 # Customize ports
-CLIENT_TOML=$HOME/.sidechain/config/client.toml
+CLIENT_TOML=$HOME/.side/config/client.toml
 sed -i.bak -e "s/^external_address *=.*/external_address = \"$(wget -qO- eth0.me):$PORT_PPROF_LADDR\"/" $CONFIG_TOML
 sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:$PORT_PROXY_APP\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:$PORT_RPC\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:$PORT_P2P\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:$PORT_PPROF_LADDR\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":$PORT_PROMETHEUS\"%" $CONFIG_TOML && \
 sed -i.bak -e "s%^address = \"localhost:9090\"%address = \"0.0.0.0:$PORT_GRPC\"%; s%^address = \"localhost:9091\"%address = \"0.0.0.0:$PORT_GRPC_WEB\"%; s%^address = \"tcp://localhost:1317\"%address = \"tcp://0.0.0.0:$PORT_API\"%" $APP_TOML && \
@@ -67,9 +67,9 @@ sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:$POR
 printGreen "Install and configure cosmovisor..." && sleep 1
 
 go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.4.0
-mkdir -p ~/.sidechain/cosmovisor/genesis/bin
-mkdir -p ~/.sidechain/cosmovisor/upgrades
-cp ~/go/bin/sided $HOME/.sidechain/cosmovisor/genesis/bin
+mkdir -p ~/.side/cosmovisor/genesis/bin
+mkdir -p ~/.side/cosmovisor/upgrades
+cp ~/go/bin/sided $HOME/.side/cosmovisor/genesis/bin
 
 printGreen "Starting service and synchronization..." && sleep 1
 
@@ -84,7 +84,7 @@ Restart=on-failure
 RestartSec=3
 LimitNOFILE=10000
 Environment="DAEMON_NAME=sided"
-Environment="DAEMON_HOME=$HOME/.sidechain"
+Environment="DAEMON_HOME=$HOME/.side"
 Environment="DAEMON_ALLOW_DOWNLOAD_BINARIES=false"
 Environment="DAEMON_RESTART_AFTER_UPGRADE=true"
 Environment="UNSAFE_SKIP_BACKUP=true"
@@ -92,12 +92,12 @@ Environment="UNSAFE_SKIP_BACKUP=true"
 WantedBy=multi-user.target
 EOF
 
-sided tendermint unsafe-reset-all --home $HOME/.sidechain --keep-addr-book
+sided tendermint unsafe-reset-all --home $HOME/.side --keep-addr-book
 
 # Add snapshot here
-URL="https://snapshots-testnet.stake-town.com/side/side-testnet-1_latest.tar.lz4"
-curl $URL | lz4 -dc - | tar -xf - -C $HOME/.sidechain
-[[ -f $HOME/.sidechain/data/upgrade-info.json ]] && cp $HOME/.sidechain/data/upgrade-info.json $HOME/.sidechain/cosmovisor/genesis/upgrade-info.json
+#URL="https://snapshots-testnet.stake-town.com/side/side-testnet-2_latest.tar.lz4"
+#curl $URL | lz4 -dc - | tar -xf - -C $HOME/.side
+#[[ -f $HOME/.side/data/upgrade-info.json ]] && cp $HOME/.side/data/upgrade-info.json $HOME/.side/cosmovisor/genesis/upgrade-info.json
 
 sudo systemctl daemon-reload
 sudo systemctl enable sided
