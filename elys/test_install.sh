616fbddadcebfb90ef43b9f23fc0367750f55e64
Yevhen Sydorenko <mogoll92@gmail.com>
2024-02-27 09:10:59 +0200

    Added elys

--- elys/test_install.sh
+++ elys/test_install.sh
@@ -12,7 +12,7 @@ read -r -p "Enter node moniker: " NODE_MONIKER
 CHAIN_ID="elystestnet-1"
 CHAIN_DENOM="uelys"
 BINARY_NAME="elysd"
-BINARY_VERSION_TAG="v0.29.15"
+BINARY_VERSION_TAG="v0.29.16"
 CHEAT_SHEET=""
 
 printDelimiter
