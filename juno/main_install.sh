112e1b4180009f97c62534ae4da38fe773c9ef9c
Yevhen Sydorenko <mogoll92@gmail.com>
2024-03-13 20:06:20 +0200

    Added side node

--- juno/main_install.sh
+++ juno/main_install.sh
@@ -12,7 +12,7 @@ read -r -p "Enter node moniker: " NODE_MONIKER
 CHAIN_ID="juno-1"
 CHAIN_DENOM="ujuno"
 BINARY_NAME="junod"
-BINARY_VERSION_TAG="v19.0.0"
+BINARY_VERSION_TAG="v21.0.0"
 CHEAT_SHEET=""
 
 printDelimiter
