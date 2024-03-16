985e9e3fd5822aaa06d232202c85fa90a159e690
Yevhen Sydorenko <mogoll92@gmail.com>
2024-03-16 12:16:43 +0200

    Added side node

--- juno/test_install.sh
+++ juno/test_install.sh
@@ -12,7 +12,7 @@ read -r -p "Enter node moniker: " NODE_MONIKER
 CHAIN_ID="uni-6"
 CHAIN_DENOM="ujunox"
 BINARY_NAME="junod"
-BINARY_VERSION_TAG="v19.0.0-alpha.3"
+BINARY_VERSION_TAG="v21.0.0-alpha.1"
 CHEAT_SHEET=""
 
 printDelimiter
