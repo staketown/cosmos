75a9869cb0934b20b9a4735d8faf2cb6eff93467
Yevhen Sydorenko <mogoll92@gmail.com>
2024-03-20 09:46:17 +0200

    Updated PF

--- umee/price_feeder_install.sh
+++ umee/price_feeder_install.sh
@@ -21,7 +21,7 @@ do
         "${options[0]}")
             # umee-1
             echo "$selected $opt"
-            VERSION=umee/v2.4.0
+            VERSION=umee/v2.4.1
             GAS_PREVOTE=55000
             GAS_VOTE=160000
             CHAIN_ID=$opt
