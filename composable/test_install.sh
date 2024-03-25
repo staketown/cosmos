261331694faead1e787b3b432d9f22611864aa99
Yevhen Sydorenko <mogoll92@gmail.com>
2024-03-25 21:20:54 +0200

    Updated composable

--- composable/test_install.sh
+++ composable/test_install.sh
@@ -12,7 +12,7 @@ read -r -p "Enter node moniker: " NODE_MONIKER
 CHAIN_ID="banksy-testnet-5"
 CHAIN_DENOM="ppica"
 BINARY_NAME="centaurid"
-BINARY_VERSION_TAG="v6_4_9"
+BINARY_VERSION_TAG="v6.4.9"
 CHEAT_SHEET="https://nodes.stake-town.com/composable"
 
 printDelimiter
