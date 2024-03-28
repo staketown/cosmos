cfcea1144cbc49865e6c58ed62b337e9d3ef98eb
Yevhen Sydorenko <mogoll92@gmail.com>
2024-03-28 06:35:55 +0200

    Updated celestia

--- elys/test_install.sh
+++ elys/test_install.sh
@@ -12,7 +12,7 @@ read -r -p "Enter node moniker: " NODE_MONIKER
 CHAIN_ID="elystestnet-1"
 CHAIN_DENOM="uelys"
 BINARY_NAME="elysd"
-BINARY_VERSION_TAG="v0.29.28"
+BINARY_VERSION_TAG="v0.29.29"
 CHEAT_SHEET=""
 
 printDelimiter
