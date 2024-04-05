3d2f859e819a1ef4ffa79edb5cfdbbcebcfa3988
Yevhen Sydorenko <mogoll92@gmail.com>
2024-04-05 16:13:24 +0300

    Updated persistence testnet

--- persistence/test_install.sh
+++ persistence/test_install.sh
@@ -9,10 +9,10 @@ export -f selectPortSet && selectPortSet
 
 read -r -p "Enter node moniker: " NODE_MONIKER
 
-CHAIN_ID="quasar-test-1"
-CHAIN_DENOM="uqsr"
-BINARY_NAME="quasarnoded"
-BINARY_VERSION_TAG="v1.0.0-rc1-testnet"
+CHAIN_ID="test-core-2"
+CHAIN_DENOM="uxprt"
+BINARY_NAME="persistenceCore"
+BINARY_VERSION_TAG="v11.7.0-fix"
 CHEAT_SHEET=""
 
 printDelimiter
@@ -28,26 +28,26 @@ echo "" && printGreen "Building binaries..." && sleep 1
 
 cd $HOME || return
 rm -rf quasar
-git clone https://github.com/quasar-finance/quasar.git
+git clone https://github.com/persistenceOne/persistenceCore
 cd quasar || return
 git checkout $BINARY_VERSION_TAG
 
 make install
 
-quasarnoded config keyring-backend os
-quasarnoded config chain-id $CHAIN_ID
-quasarnoded init "$NODE_MONIKER" --chain-id $CHAIN_ID
+persistenceCore config keyring-backend os
+persistenceCore config chain-id $CHAIN_ID
+persistenceCore init "$NODE_MONIKER" --chain-id $CHAIN_ID
 
-curl -Ls https://snapshots-testnet.stake-town.com/quasar/genesis.json > $HOME/.quasarnode/config/genesis.json
-curl -Ls https://snapshots-testnet.stake-town.com/quasar/addrbook.json > $HOME/.quasarnode/config/addrbook.json
+curl -Ls https://snapshots-testnet.stake-town.com/persistence/genesis.json > $HOME/.persistenceCore/config/genesis.json
+curl -Ls https://snapshots-testnet.stake-town.com/persistence/addrbook.json > $HOME/.persistenceCore/config/addrbook.json
 
-CONFIG_TOML=$HOME/.quasarnode/config/config.toml
-SEEDS="3f472746f46493309650e5a033076689996c8881@quasar-testnet.rpc.kjnodes.com:14859"
+CONFIG_TOML=$HOME/.persistenceCore/config/config.toml
+SEEDS="ade4d8bc8cbe014af6ebdf3cb7b1e9ad36f412c0@testnet-seeds.polkachu.com:15456"
 PEERS=""
 sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $CONFIG_TOML
 sed -i.bak -e "s/^seeds =.*/seeds = \"$SEEDS\"/" $CONFIG_TOML
 
-APP_TOML=$HOME/.quasarnode/config/app.toml
+APP_TOML=$HOME/.persistenceCore/config/app.toml
 sed -i 's|^pruning *=.*|pruning = "custom"|g' $APP_TOML
 sed -i 's|^pruning-keep-recent  *=.*|pruning-keep-recent = "100"|g' $APP_TOML
 sed -i 's|^pruning-keep-every *=.*|pruning-keep-every = "0"|g' $APP_TOML
@@ -55,10 +55,10 @@ sed -i 's|^pruning-interval *=.*|pruning-interval = "19"|g' $APP_TOML
 sed -i -e "s/^filter_peers *=.*/filter_peers = \"true\"/" $CONFIG_TOML
 indexer="null"
 sed -i -e "s/^indexer *=.*/indexer = \"$indexer\"/" $CONFIG_TOML
-sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0uqsr"|g' $APP_TOML
+sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0uxprt"|g' $APP_TOML
 
 # Customize ports
-CLIENT_TOML=$HOME/.quasarnode/config/client.toml
+CLIENT_TOML=$HOME/.persistenceCore/config/client.toml
 sed -i.bak -e "s/^external_address *=.*/external_address = \"$(wget -qO- eth0.me):$PORT_PPROF_LADDR\"/" $CONFIG_TOML
 sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:$PORT_PROXY_APP\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:$PORT_RPC\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:$PORT_P2P\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:$PORT_PPROF_LADDR\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":$PORT_PROMETHEUS\"%" $CONFIG_TOML && \
 sed -i.bak -e "s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:$PORT_GRPC\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:$PORT_GRPC_WEB\"%; s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:$PORT_API\"%" $APP_TOML && \
@@ -67,13 +67,13 @@ sed -i.bak -e "s%^node = \"tcp://127.0.0.1:26657\"%node = \"tcp://127.0.0.1:$POR
 printGreen "Install and configure cosmovisor..." && sleep 1
 
 go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.4.0
-mkdir -p ~/.quasarnode/cosmovisor/genesis/bin
-mkdir -p ~/.quasarnode/cosmovisor/upgrades
-cp ~/go/bin/quasarnoded $HOME/.quasarnode/cosmovisor/genesis/bin
+mkdir -p ~/.persistenceCore/cosmovisor/genesis/bin
+mkdir -p ~/.persistenceCore/cosmovisor/upgrades
+cp ~/go/bin/persistenceCore $HOME/.persistenceCore/cosmovisor/genesis/bin
 
 printGreen "Starting service and synchronization..." && sleep 1
 
-sudo tee /etc/systemd/system/quasarnoded.service > /dev/null << EOF
+sudo tee /etc/systemd/system/persistenceCore.service > /dev/null << EOF
 [Unit]
 Description=Quasar Node
 After=network-online.target
@@ -83,8 +83,8 @@ ExecStart=$(which cosmovisor) run start
 Restart=on-failure
 RestartSec=3
 LimitNOFILE=10000
-Environment="DAEMON_NAME=quasarnoded"
-Environment="DAEMON_HOME=$HOME/.quasarnode"
+Environment="DAEMON_NAME=persistenceCore"
+Environment="DAEMON_HOME=$HOME/.persistenceCore"
 Environment="DAEMON_ALLOW_DOWNLOAD_BINARIES=false"
 Environment="DAEMON_RESTART_AFTER_UPGRADE=true"
 Environment="UNSAFE_SKIP_BACKUP=true"
@@ -92,12 +92,13 @@ Environment="UNSAFE_SKIP_BACKUP=true"
 WantedBy=multi-user.target
 EOF
 
-quasarnoded tendermint unsafe-reset-all --home $HOME/.quasarnode --keep-addr-book
+persistenceCore tendermint unsafe-reset-all --home $HOME/.persistenceCore --keep-addr-book
 
 # Add snapshot here
-URL=https://snapshots-testnet.stake-town.com/quasar/quasar-test-1_latest.tar.lz4
-curl -L $URL | lz4 -dc - | tar -xf - -C $HOME/.quasarnode
-[[ -f $HOME/.quasarnode/data/upgrade-info.json ]] && cp $HOME/.quasarnode/data/upgrade-info.json $HOME/.quasarnode/cosmovisor/genesis/upgrade-info.json
+#URL=https://snapshots-testnet.stake-town.com/quasar/quasar-test-1_latest.tar.lz4
+URL=
+curl -L $URL | lz4 -dc - | tar -xf - -C $HOME/.persistenceCore
+[[ -f $HOME/.persistenceCore/data/upgrade-info.json ]] && cp $HOME/.persistenceCore/data/upgrade-info.json $HOME/.persistenceCore/cosmovisor/genesis/upgrade-info.json
 
 sudo systemctl daemon-reload
 sudo systemctl enable $BINARY_NAME
