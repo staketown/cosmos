ed92077b67d0b3924a38e679b5af16c1c36fe623
Yevhen Sydorenko <mogoll92@gmail.com>
2024-04-05 17:40:52 +0300

    Updated persistence

--- persistence/test_install.sh
+++ persistence/test_install.sh
@@ -12,7 +12,7 @@ read -r -p "Enter node moniker: " NODE_MONIKER
 CHAIN_ID="test-core-2"
 CHAIN_DENOM="uxprt"
 BINARY_NAME="persistenceCore"
-BINARY_VERSION_TAG="v11.7.0-fix"
+BINARY_VERSION_TAG="v11.9.0-rc0"
 CHEAT_SHEET=""
 
 printDelimiter
