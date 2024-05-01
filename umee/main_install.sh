9584e04b850bac8aeefdd04ecbf5b94770aac8d0
Yevhen Sydorenko <mogoll92@gmail.com>
2024-05-01 20:15:40 +0300

    Updated pica

--- umee/main_install.sh
+++ umee/main_install.sh
@@ -12,7 +12,7 @@ read -r -p "Enter node moniker: " NODE_MONIKER
 CHAIN_ID="umee-1"
 CHAIN_DENOM="uumee"
 BINARY_NAME="umeed"
-BINARY_VERSION_TAG="v6.4.0"
+BINARY_VERSION_TAG="v6.4.1"
 CHEAT_SHEET=""
 
 printDelimiter
