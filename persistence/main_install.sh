2a55200aa3c4a76025cf7e6970bbce143fce0ccb
Yevhen Sydorenko <mogoll92@gmail.com>
2024-05-02 19:25:44 +0300

    Updated persistence

--- persistence/main_install.sh
+++ persistence/main_install.sh
@@ -12,7 +12,7 @@ read -r -p "Enter node moniker: " NODE_MONIKER
 CHAIN_ID="core-1"
 CHAIN_DENOM="uxprt"
 BINARY_NAME="persistenceCore"
-BINARY_VERSION_TAG="v11.10.0"
+BINARY_VERSION_TAG="v11.11.0"
 CHEAT_SHEET=""
 
 printDelimiter
