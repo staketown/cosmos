5e4373a1d3131aa9d1092e3c56eae111fbb15c1d
Yevhen Sydorenko <mogoll92@gmail.com>
2024-05-21 14:59:06 +0300

    Added neutron mainnet script

---
+++ neutron/main_install.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+source <(curl -s https://raw.githubusercontent.com/staketown/utils/master/common.sh)
+
+printLogo
+
+source <(curl -s https://raw.githubusercontent.com/staketown/cosmos/master/utils/ports.sh) && sleep 1
+export -f selectPortSet && selectPortSet
+
+read -r -p "Enter node moniker: " NODE_MONIKER
+
+CHAIN_ID="neutron-1"
+CHAIN_DENOM="untrn"
+BINARY_NAME="neutrond"
+BINARY_VERSION_TAG="v3.0.2"
+CHEAT_SHEET=""
+
+printDelimiter
+echo -e "Node moniker:       $NODE_MONIKER"
+echo -e "Chain id:           $CHAIN_ID"
+echo -e "Chain demon:        $CHAIN_DENOM"
+echo -e "Binary version tag: $BINARY_VERSION_TAG"
+printDelimiter && sleep 1
+
+source <(curl -s https://raw.githubusercontent.com/staketown/cosmos/master/utils/dependencies.sh)
+
+echo "" && printGreen "Building binaries..." && sleep 1
+
+cd $HOME || return
+rm -rf neutron
+git clone https://github.com/neutron-org/neutron.git
+cd $HOME/neutron || return
+git checkout $BINARY_VERSION_TAG
+
+make install
+
+neutrond config keyring-backend os
+neutrond config chain-id $CHAIN_ID
+neutrond init "$NODE_MONIKER" --chain-id $CHAIN_ID
+
+curl -Ls https://snapshots.polkachu.com/genesis/neutron/genesis.json > $HOME/.neutrond/config/genesis.json
+#curl -Ls https://snapshots.stake-town.com/source/addrbook.json > $HOME/.neutrond/config/addrbook.json
+
+CONFIG_TOML=$HOME/.neutrond/config/config.toml
+PEERS=""
+sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $CONFIG_TOML
+SEEDS="ade4d8bc8cbe014af6ebdf3cb7b1e9ad36f412c0@seeds.polkachu.com:19156"
+sed -i.bak -e "s/^seeds =.*/seeds = \"$SEEDS\"/" $CONFIG_TOML
+
+APP_TOML=$HOME/.neutrond/config/app.toml
+sed -i 's|^pruning *=.*|pruning = "custom"|g' $APP_TOML
+sed -i 's|^pruning-keep-recent  *=.*|pruning-keep-recent = "100"|g' $APP_TOML
+sed -i 's|^pruning-keep-every *=.*|pruning-keep-every = "0"|g' $APP_TOML
+sed -i 's|^pruning-interval *=.*|pruning-interval = "19"|g' $APP_TOML
+sed -i -e "s/^filter_peers *=.*/filter_peers = \"true\"/" $CONFIG_TOML
+indexer="null"
+sed -i -e "s/^indexer *=.*/indexer = \"$indexer\"/" $CONFIG_TOML
+
+sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.001untrn"|g' $APP_TOML
+
+# Customize ports
+CLIENT_TOML=$HOME/.neutrond/config/client.toml
+sed -i.bak -e "s/^external_address *=.*/external_address = \"$(wget -qO- eth0.me):$PORT_PPROF_LADDR\"/" $CONFIG_TOML
+sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:$PORT_PROXY_APP\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:$PORT_RPC\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:$PORT_P2P\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:$PORT_PPROF_LADDR\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":$PORT_PROMETHEUS\"%" $CONFIG_TOML && \
+sed -i.bak -e "s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:$PORT_GRPC\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:$PORT_GRPC_WEB\"%; s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:$PORT_API\"%" $APP_TOML && \
+sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:$PORT_RPC\"%" $CLIENT_TOML
+
+printGreen "Install and configure cosmovisor..." && sleep 1
+
+go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.4.0
+mkdir -p ~/.neutrond/cosmovisor/genesis/bin
+mkdir -p ~/.neutrond/cosmovisor/upgrades
+cp ~/go/bin/neutrond $HOME/.neutrond/cosmovisor/genesis/bin
+
+printGreen "Starting service and synchronization..." && sleep 1
+
+sudo tee /etc/systemd/system/neutrond.service > /dev/null << EOF
+[Unit]
+Description=Neutron Node
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=$(which cosmovisor) run start
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=10000
+Environment="DAEMON_NAME=neutrond"
+Environment="DAEMON_HOME=$HOME/.neutrond"
+Environment="DAEMON_ALLOW_DOWNLOAD_BINARIES=false"
+Environment="DAEMON_RESTART_AFTER_UPGRADE=true"
+Environment="UNSAFE_SKIP_BACKUP=true"
+[Install]
+WantedBy=multi-user.target
+EOF
+
+neutrond tendermint unsafe-reset-all --home $HOME/.source --keep-addr-book
+
+# Add snapshot here
+URL="https://snapshots.polkachu.com/snapshots/neutron/neutron_10443706.tar.lz4"
+curl -L $URL | lz4 -dc - | tar -xf - -C $HOME/.neutrond
+
+sudo systemctl daemon-reload
+sudo systemctl enable neutrond
+sudo systemctl start neutrond
+
+printDelimiter
+printGreen "Check logs:            sudo journalctl -u $BINARY_NAME -f -o cat"
+printGreen "Check synchronization: $BINARY_NAME status 2>&1 | jq .SyncInfo.catching_up"
+printGreen "Check our cheat sheet: $CHEAT_SHEET"
+printDelimiter
