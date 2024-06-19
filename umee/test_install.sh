70fc10cb0dc401ea8d38afae1b07a50b5953f0a9
Yevhen Sydorenko <mogoll92@gmail.com>
2024-06-19 14:49:18 +0300

    Updated umee

--- umee/test_install.sh
+++ umee/test_install.sh
@@ -12,7 +12,7 @@ read -r -p "Enter node moniker: " NODE_MONIKER
 CHAIN_ID="canon-4"
 CHAIN_DENOM="uumee"
 BINARY_NAME="umeed"
-BINARY_VERSION_TAG="v6.4.1"
+BINARY_VERSION_TAG="v6.5.0-rc2"
 CHEAT_SHEET=""
 
 printDelimiter
