5730e428d31c92c1fbab056c0da72ade52bb20a7
Yevhen Sydorenko <mogoll92@gmail.com>
2024-06-20 08:34:01 +0300

    Added akash

--- akash/main_install.sh
+++ akash/main_install.sh
@@ -9,10 +9,10 @@ export -f selectPortSet && selectPortSet
 
 read -r -p "Enter node moniker: " NODE_MONIKER
 
-CHAIN_ID="cataclysm-1"
-CHAIN_DENOM="unibi"
-BINARY_NAME="nibid"
-BINARY_VERSION_TAG="v1.0.0"
+CHAIN_ID="akashnet-2"
+CHAIN_DENOM="uakt"
+BINARY_NAME="akash"
+BINARY_VERSION_TAG="v0.36.0"
 CHEAT_SHEET=""
 
 printDelimiter
@@ -27,30 +27,26 @@ source <(curl -s https://raw.githubusercontent.com/staketown/cosmos/master/utils
 echo "" && printGreen "Building binaries..." && sleep 1
 
 cd $HOME || return
-rm -rf nibiru
-git clone https://github.com/NibiruChain/nibiru
-cd $HOME/nibiru || return
+rm -rf akash
+git clone https://github.com/ovrclk/akash.git
+cd $HOME/akash || return
 git checkout $BINARY_VERSION_TAG
 
 make install
 
-nibid config keyring-backend os
-nibid config chain-id $CHAIN_ID
-nibid init "$NODE_MONIKER" --chain-id $CHAIN_ID
+akash config keyring-backend os
+akash config chain-id $CHAIN_ID
+akash init "$NODE_MONIKER" --chain-id $CHAIN_ID
 
-curl -Ls https://snapshots.kjnodes.com/nibiru/genesis.json > $HOME/.nibid/config/genesis.json
-curl -Ls https://snapshots.kjnodes.com/nibiru/addrbook.json > $HOME/.nibid/config/addrbook.json
+curl -Ls https://snapshots.polkachu.com/genesis/akash/genesis.json > $HOME/.akash/config/genesis.json
 
-#curl -s https://snapshots-testnet.stake-town.com/nibiru/genesis.json > $HOME/.nibid/config/genesis.json
-#curl -s https://snapshots-testnet.stake-town.com/nibiru/addrbook.json > $HOME/.nibid/config/addrbook.json
-
-CONFIG_TOML=$HOME/.nibid/config/config.toml
+CONFIG_TOML=$HOME/.akash/config/config.toml
 PEERS=""
 sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $CONFIG_TOML
-SEEDS="400f3d9e30b69e78a7fb891f60d76fa3c73f0ecc@nibiru.rpc.kjnodes.com:13959"
+SEEDS="ade4d8bc8cbe014af6ebdf3cb7b1e9ad36f412c0@seeds.polkachu.com:12856"
 sed -i.bak -e "s/^seeds =.*/seeds = \"$SEEDS\"/" $CONFIG_TOML
 
-APP_TOML=$HOME/.nibid/config/app.toml
+APP_TOML=$HOME/.akash/config/app.toml
 sed -i 's|^pruning *=.*|pruning = "custom"|g' $APP_TOML
 sed -i 's|^pruning-keep-recent  *=.*|pruning-keep-recent = "100"|g' $APP_TOML
 sed -i 's|^pruning-keep-every *=.*|pruning-keep-every = "0"|g' $APP_TOML
@@ -58,10 +54,10 @@ sed -i 's|^pruning-interval *=.*|pruning-interval = "19"|g' $APP_TOML
 sed -i -e "s/^filter_peers *=.*/filter_peers = \"true\"/" $CONFIG_TOML
 indexer="null"
 sed -i -e "s/^indexer *=.*/indexer = \"$indexer\"/" $CONFIG_TOML
-sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.025unibi"|g' $APP_TOML
+sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.0uakt"|g' $APP_TOML
 
 # Customize ports
-CLIENT_TOML=$HOME/.nibid/config/client.toml
+CLIENT_TOML=$HOME/.akash/config/client.toml
 sed -i.bak -e "s/^external_address *=.*/external_address = \"$(wget -qO- eth0.me):$PORT_PPROF_LADDR\"/" $CONFIG_TOML
 sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:$PORT_PROXY_APP\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:$PORT_RPC\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:$PORT_P2P\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:$PORT_PPROF_LADDR\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":$PORT_PROMETHEUS\"%" $CONFIG_TOML && \
 sed -i.bak -e "s%^address = \"localhost:9090\"%address = \"localhost:$PORT_GRPC\"%; s%^address = \"localhost:9091\"%address = \"localhost:$PORT_GRPC_WEB\"%; s%^address = \"tcp://localhost:1317\"%address = \"tcp://localhost:$PORT_API\"%" $APP_TOML && \
@@ -70,15 +66,15 @@ sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:$POR
 printGreen "Install and configure cosmovisor..." && sleep 1
 
 go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.4.0
-mkdir -p ~/.nibid/cosmovisor/genesis/bin
-mkdir -p ~/.nibid/cosmovisor/upgrades
-cp ~/go/bin/nibid $HOME/.nibid/cosmovisor/genesis/bin
+mkdir -p ~/.akash/cosmovisor/genesis/bin
+mkdir -p ~/.akash/cosmovisor/upgrades
+cp ~/go/bin/akash $HOME/.akash/cosmovisor/genesis/bin
 
 printGreen "Starting service and synchronization..." && sleep 1
 
-sudo tee /etc/systemd/system/nibid.service > /dev/null << EOF
+sudo tee /etc/systemd/system/akash.service > /dev/null << EOF
 [Unit]
-Description=Nibiru Node
+Description=Akash Node
 After=network-online.target
 [Service]
 User=$USER
@@ -86,8 +82,8 @@ ExecStart=$(which cosmovisor) run start
 Restart=on-failure
 RestartSec=3
 LimitNOFILE=10000
-Environment="DAEMON_NAME=nibid"
-Environment="DAEMON_HOME=$HOME/.nibid"
+Environment="DAEMON_NAME=akash"
+Environment="DAEMON_HOME=$HOME/.akash"
 Environment="DAEMON_ALLOW_DOWNLOAD_BINARIES=false"
 Environment="DAEMON_RESTART_AFTER_UPGRADE=true"
 Environment="UNSAFE_SKIP_BACKUP=true"
@@ -95,17 +91,17 @@ Environment="UNSAFE_SKIP_BACKUP=true"
 WantedBy=multi-user.target
 EOF
 
-nibid tendermint unsafe-reset-all --home $HOME/.nibid --keep-addr-book
+akash tendermint unsafe-reset-all --home $HOME/.akash --keep-addr-book
 
 # Add snapshot here
 #URL="https://snapshots-testnet.stake-town.com/nibiru/nibiru-itn-3_latest.tar.lz4"
 #curl $URL | lz4 -dc - | tar -xf - -C $HOME/.nibid
-curl -L https://snapshots.kjnodes.com/nibiru/snapshot_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.nibid
-[[ -f $HOME/.nibid/data/upgrade-info.json ]]  && cp $HOME/.nibid/data/upgrade-info.json $HOME/.nibid/cosmovisor/genesis/upgrade-info.json
+curl -L https://snapshots.polkachu.com/snapshots/akash/akash_16815570.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.akash
+[[ -f $HOME/.akash/data/upgrade-info.json ]]  && cp $HOME/.akash/data/upgrade-info.json $HOME/.akash/cosmovisor/genesis/upgrade-info.json
 
 sudo systemctl daemon-reload
-sudo systemctl enable nibid
-sudo systemctl start nibid
+sudo systemctl enable akash
+sudo systemctl start akash
 
 printDelimiter
 printGreen "Check logs:            sudo journalctl -u $BINARY_NAME -f -o cat"
