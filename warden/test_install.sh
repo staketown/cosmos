b71bf5ccfcad19d2529f89878915c2b6198d102c
Yevhen Sydorenko <mogoll92@gmail.com>
2024-08-13 14:48:17 +0300

    Updated version

--- warden/test_install.sh
+++ warden/test_install.sh
@@ -12,7 +12,7 @@ read -r -p "Enter node moniker: " NODE_MONIKER
 CHAIN_ID="buenavista-1"
 CHAIN_DENOM="uward"
 BINARY_NAME="wardend"
-BINARY_VERSION_TAG="v0.3.2"
+BINARY_VERSION_TAG="v0.4.1"
 CHEAT_SHEET=""
 
 printDelimiter
