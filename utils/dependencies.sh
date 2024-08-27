bdb0dee78eba9afddead1fd7bbb3357a594f76a1
Yevhen Sydorenko <mogoll92@gmail.com>
2024-08-27 12:28:02 +0300

    Updated dependencies

--- utils/dependencies.sh
+++ utils/dependencies.sh
@@ -5,7 +5,8 @@ source <(curl -s https://raw.githubusercontent.com/staketown/utils/master/common
 printGreen "Installing dependencies..." && sleep 1
 sudo apt-get update &&
 sudo apt-get install -y curl iptables build-essential git lz4 wget jq make gcc nano chrony \
-tmux htop nvme-cli pkg-config libssl-dev libleveldb-dev tar clang bsdmainutils ncdu unzip libleveldb-dev aria2 &&
+tmux htop nvme-cli pkg-config libssl-dev libleveldb-dev tar clang bsdmainutils ncdu unzip \
+libleveldb-dev aria2 net-tools &&
 
 printGreen "Installing go..." && sleep 1
 if ! [ -x "$(command -v go)" ]; then
