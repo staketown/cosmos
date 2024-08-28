c8645a46c75a31f97c87c517c0d412f216973d01
Yevhen Sydorenko <mogoll92@gmail.com>
2024-08-28 13:00:56 +0300

    Updated lava

--- lava/test_install.sh
+++ lava/test_install.sh
@@ -12,7 +12,7 @@ read -r -p "Enter node moniker: " NODE_MONIKER
 CHAIN_ID="lava-testnet-2"
 CHAIN_DENOM="ulava"
 BINARY_NAME="lavad"
-BINARY_VERSION_TAG="v2.5.0"
+BINARY_VERSION_TAG="v3.0.1"
 CHEAT_SHEET=""
 
 printDelimiter
