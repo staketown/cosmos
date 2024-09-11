4920ff9918dcbb9315fb41c0a371731089f7447f
Yevhen Sydorenko <mogoll92@gmail.com>
2024-09-11 15:58:00 +0300

    Added axone protocol node

--- axone/test_install.sh
+++ axone/test_install.sh
@@ -9,10 +9,10 @@ export -f selectPortSet && selectPortSet
 
 read -r -p "Enter node moniker: " NODE_MONIKER
 
-CHAIN_ID="union-testnet-4"
-CHAIN_DENOM="muno"
-BINARY_NAME="uniond"
-BINARY_VERSION_TAG="v0.15.0"
+CHAIN_ID="axone-dentrite-1"
+CHAIN_DENOM="uaxone"
+BINARY_NAME="axoned"
+BINARY_VERSION_TAG="v10.0.0"
 CHEAT_SHEET=""
 
 printDelimiter
@@ -26,26 +26,29 @@ source <(curl -s https://raw.githubusercontent.com/staketown/cosmos/master/utils
 
 echo "" && printGreen "Building binaries..." && sleep 1
 
-wget -O $HOME/go/bin/uniond https://snapshots.kjnodes.com/union-testnet/uniond-0.15.0-linux-amd64
-chmod +x $HOME/go/bin/uniond
+cd $HOME || return
+rm -rf axoned
+git clone https://github.com/axone-protocol/axoned.git
+cd axoned || return
+git checkout $BINARY_VERSION_TAG
 
-uniond config keyring-backend os
-uniond config chain-id $CHAIN_ID
-uniond init "$NODE_MONIKER" bn254 --chain-id $CHAIN_ID
+make install
+
+axoned config keyring-backend os
+axoned config chain-id $CHAIN_ID
+axoned init "$NODE_MONIKER" bn254 --chain-id $CHAIN_ID
 
 # Download genesis and addrbook
-curl -Ls https://snapshots.kjnodes.com/union-testnet/genesis.json > $HOME/.union/config/genesis.json
-curl -Ls https://snapshots.kjnodes.com/union-testnet/addrbook.json > $HOME/.union/config/addrbook.json
-#curl -Ls https://snapshots-testnet.stake-town.com/ojo/genesis.json > $HOME/.ojo/config/genesis.json
-#curl -Ls https://snapshots-testnet.stake-town.com/ojo/addrbook.json > $HOME/.ojo/config/addrbook.json
+curl -Ls https://snapshots.kjnodes.com/axone-testnet/genesis.json > $HOME/.axoned/config/genesis.json
+curl -Ls https://snapshots.kjnodes.com/axone-testnet/addrbook.json > $HOME/.axoned/config/addrbook.json
 
-CONFIG_TOML=$HOME/.union/config/config.toml
+CONFIG_TOML=$HOME/.axoned/config/config.toml
 PEERS=""
 sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $CONFIG_TOML
-SEEDS="3f472746f46493309650e5a033076689996c8881@union-testnet.rpc.kjnodes.com:17159"
+SEEDS="3f472746f46493309650e5a033076689996c8881@axone-testnet.rpc.kjnodes.com:13659"
 sed -i.bak -e "s/^seeds =.*/seeds = \"$SEEDS\"/" $CONFIG_TOML
 
-APP_TOML=$HOME/.union/config/app.toml
+APP_TOML=$HOME/.axoned/config/app.toml
 sed -i 's|^pruning *=.*|pruning = "custom"|g' $APP_TOML
 sed -i 's|^pruning-keep-recent  *=.*|pruning-keep-recent = "100"|g' $APP_TOML
 sed -i 's|^pruning-keep-every *=.*|pruning-keep-every = "0"|g' $APP_TOML
@@ -53,27 +56,27 @@ sed -i 's|^pruning-interval *=.*|pruning-interval = "19"|g' $APP_TOML
 sed -i -e "s/^filter_peers *=.*/filter_peers = \"true\"/" $CONFIG_TOML
 indexer="null"
 sed -i -e "s/^indexer *=.*/indexer = \"$indexer\"/" $CONFIG_TOML
-sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0muno"|g' $APP_TOML
+sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0uaxone"|g' $APP_TOML
 
 # Customize ports
-CLIENT_TOML=$HOME/.union/config/client.toml
+CLIENT_TOML=$HOME/.axoned/config/client.toml
 sed -i.bak -e "s/^external_address *=.*/external_address = \"$(wget -qO- eth0.me):$PORT_PPROF_LADDR\"/" $CONFIG_TOML
 sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:$PORT_PROXY_APP\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:$PORT_RPC\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:$PORT_P2P\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:$PORT_PPROF_LADDR\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":$PORT_PROMETHEUS\"%" $CONFIG_TOML && \
-sed -i.bak -e "s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:$PORT_GRPC\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:$PORT_GRPC_WEB\"%; s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:$PORT_API\"%" $APP_TOML && \
+sed -i.bak -e "s%^address = \"localhost:9090\"%address = \"0.0.0.0:$PORT_GRPC\"%; s%^address = \"localhost:9091\"%address = \"0.0.0.0:$PORT_GRPC_WEB\"%; s%^address = \"tcp://localhost:1317\"%address = \"tcp://0.0.0.0:$PORT_API\"%" $APP_TOML && \
 sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:$PORT_RPC\"%" $CLIENT_TOML
 
 printGreen "Install and configure cosmovisor..." && sleep 1
 
 go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.4.0
-mkdir -p ~/.union/cosmovisor/genesis/bin
-mkdir -p ~/.union/cosmovisor/upgrades
-cp ~/go/bin/uniond $HOME/.union/cosmovisor/genesis/bin
+mkdir -p ~/.axoned/cosmovisor/genesis/bin
+mkdir -p ~/.axoned/cosmovisor/upgrades
+cp ~/go/bin/axoned $HOME/.axoned/cosmovisor/genesis/bin
 
 printGreen "Starting service and synchronization..." && sleep 1
 
-sudo tee /etc/systemd/system/uniond.service > /dev/null << EOF
+sudo tee /etc/systemd/system/axoned.service > /dev/null << EOF
 [Unit]
-Description=Union Node
+Description=Axone Node
 After=network-online.target
 [Service]
 User=$USER
@@ -81,8 +84,8 @@ ExecStart=$(which cosmovisor) run start
 Restart=on-failure
 RestartSec=3
 LimitNOFILE=10000
-Environment="DAEMON_NAME=uniond"
-Environment="DAEMON_HOME=$HOME/.union"
+Environment="DAEMON_NAME=axoned"
+Environment="DAEMON_HOME=$HOME/.axoned"
 Environment="DAEMON_ALLOW_DOWNLOAD_BINARIES=false"
 Environment="DAEMON_RESTART_AFTER_UPGRADE=true"
 Environment="UNSAFE_SKIP_BACKUP=true"
@@ -90,15 +93,15 @@ Environment="UNSAFE_SKIP_BACKUP=true"
 WantedBy=multi-user.target
 EOF
 
-uniond tendermint unsafe-reset-all --home $HOME/.union --keep-addr-book
+axoned tendermint unsafe-reset-all --home $HOME/.union --keep-addr-book
 
 # Add snapshot here
-curl -L https://snapshots.kjnodes.com/union-testnet/snapshot_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.union
-[[ -f $HOME/.union/data/upgrade-info.json ]] && cp $HOME/.union/data/upgrade-info.json $HOME/.union/cosmovisor/genesis/upgrade-info.json
+curl -L https://snapshots.kjnodes.com/axone-testnet/snapshot_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.axoned
+[[ -f $HOME/.axoned/data/upgrade-info.json ]] && cp $HOME/.axoned/data/upgrade-info.json $HOME/.axoned/cosmovisor/genesis/upgrade-info.json
 
 sudo systemctl daemon-reload
-sudo systemctl enable uniond
-sudo systemctl start uniond
+sudo systemctl enable axoned
+sudo systemctl start axoned
 
 printDelimiter
 printGreen "Check logs:            sudo journalctl -u $BINARY_NAME -f -o cat"
