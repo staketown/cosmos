f7e5972a688f59f976d8b7f155f731ab732165a5
Yevhen Sydorenko <mogoll92@gmail.com>
2024-09-30 20:12:06 +0300

    Updated c4e

--- c4e/main_install.sh
+++ c4e/main_install.sh
@@ -12,7 +12,7 @@ read -r -p "Enter node moniker: " NODE_MONIKER
 CHAIN_ID="perun-1"
 CHAIN_DENOM="uc4e"
 BINARY_NAME="c4ed"
-BINARY_VERSION_TAG="v1.4.1"
+BINARY_VERSION_TAG="v1.4.2"
 CHEAT_SHEET=""
 
 printDelimiter
