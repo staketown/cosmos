df25aed8f57048c15d2519d03d6e744b3a8cebc3
Yevhen Sydorenko <mogoll92@gmail.com>
2024-10-26 17:02:28 +0300

    Updated side

--- side/test_install.sh
+++ side/test_install.sh
@@ -12,7 +12,7 @@ read -r -p "Enter node moniker: " NODE_MONIKER
 CHAIN_ID="sidechain-testnet-4"
 CHAIN_DENOM="uside"
 BINARY_NAME="sided"
-BINARY_VERSION_TAG="v0.9.3"
+BINARY_VERSION_TAG="v0.9.4"
 CHEAT_SHEET=""
 
 printDelimiter
