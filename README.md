2595e74e8a6a60584cbeabfb4d3ff7348df0c44c
Yevhen Sydorenko <mogoll92@gmail.com>
2025-01-03 16:07:15 +0200

    Added native network

--- README.md
+++ README.md
@@ -14,35 +14,35 @@ bash <(curl -s https://raw.githubusercontent.com/staketown/cosmos/master/composa
 
 ## Network prefixes
 
-| Network       | Index |
-|---------------|:-----:|
-| Gitopia       |   1   |
-| Akash         |   2   |
-| Source        |   3   |
-| Umee          |   4   |
-| Lava          |   5   |
-| Archway       |   6   |
-| CosmosHub     |   7   |
-| Juno          |   8   |
-| Celestia      |   9   |
-| C4E           |  10   |
-| Ojo           |  11   |
-| Composable    |  12   |
-| Elys          |  13   |
-| Warden        |  14   |
-| Evmos         |  15   |
-| Osmosis       |  16   |
-| Arkeo Network |  17   |
-| Axelar        |  18   |
-| Prysm         |  19   |
-| Quasar        |  20   |
-| Aura          |  21   |
-| Mantra        |  22   |
-| OG            |  23   |
-| Side Protocol |  24   |
-| QuickSilver   |  25   |
-| Planq         |  26   |
-| CrossFi       |  27   |
-| Persistence   |  28   |
-| Neutron       |  29   |
-| Exocore       |  30   |
+| Network        | Index |
+|----------------|:-----:|
+| Gitopia        |   1   |
+| Akash          |   2   |
+| Source         |   3   |
+| Umee           |   4   |
+| Lava           |   5   |
+| Archway        |   6   |
+| CosmosHub      |   7   |
+| Juno           |   8   |
+| Celestia       |   9   |
+| C4E            |  10   |
+| Ojo            |  11   |
+| Composable     |  12   |
+| Elys           |  13   |
+| Warden         |  14   |
+| Evmos          |  15   |
+| Osmosis        |  16   |
+| Arkeo Network  |  17   |
+| Axelar         |  18   |
+| Prysm          |  19   |
+| Quasar         |  20   |
+| Aura           |  21   |
+| Mantra         |  22   |
+| OG             |  23   |
+| Side Protocol  |  24   |
+| QuickSilver    |  25   |
+| Planq          |  26   |
+| Native Network |  27   |
+| Persistence    |  28   |
+| Neutron        |  29   |
+| Exocore        |  30   |
--- native/test_install.sh
+++ native/test_install.sh
@@ -9,10 +9,10 @@ export -f selectPortSet && selectPortSet
 
 read -r -p "Enter node moniker: " NODE_MONIKER
 
-CHAIN_ID="crossfi-evm-testnet-1"
-CHAIN_DENOM="mpx"
-BINARY_NAME="crossfid"
-BINARY_VERSION_TAG="0.3.0-prebuild9"
+CHAIN_ID="native-t1"
+CHAIN_DENOM="untiv"
+BINARY_NAME="gonative"
+BINARY_VERSION_TAG="v0.1.0"
 CHEAT_SHEET=""
 
 printDelimiter
@@ -27,26 +27,28 @@ source <(curl -s https://raw.githubusercontent.com/staketown/cosmos/master/utils
 echo "" && printGreen "Building binaries..." && sleep 1
 
 cd $HOME || return
-curl -L https://github.com/crossfichain/crossfi-node/releases/download/v0.3.0-prebuild9/crossfi-node_0.3.0-prebuild9_linux_amd64.tar.gz > crossfi-node_0.3.0-prebuild9_linux_amd64.tar.gz
-tar -xvzf crossfi-node_0.3.0-prebuild9_linux_amd64.tar.gz
-chmod +x $HOME/bin/crossfid
-mv $HOME/bin/crossfid $HOME/go/bin
-rm -rf crossfi-node_0.3.0-prebuild9_linux_amd64.tar.gz readme.md $HOME/bin
+rm -rf gonative
+git clone https://github.com/gonative-cc/gonative.git
+cd gonative || return
+git checkout $BINARY_VERSION_TAG
 
-crossfid config keyring-backend os
-crossfid config chain-id $CHAIN_ID
-crossfid init "$NODE_MONIKER" --chain-id $CHAIN_ID
+make build
+mv $HOME/gonative/out/gonative $HOME/go/bin
 
-curl -s https://snapshots-testnet.stake-town.com/crossfi/genesis.json > $HOME/.crossfid/config/genesis.json
-curl -s https://snapshots-testnet.stake-town.com/crossfi/addrbook.json > $HOME/.crossfid/config/addrbook.json
+gonative config keyring-backend os
+gonative config chain-id $CHAIN_ID
+gonative init "$NODE_MONIKER" --chain-id $CHAIN_ID
 
-CONFIG_TOML=$HOME/.crossfid/config/config.toml
+curl -s https://snapshots.polkachu.com/testnet-genesis/native/genesis.json > $HOME/.gonative/config/genesis.json
+#curl -s https://snapshots-testnet.stake-town.com/crossfi/addrbook.json > $HOME/.gonative/config/addrbook.json
+
+CONFIG_TOML=$HOME/.gonative/config/config.toml
 PEERS=""
 sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $CONFIG_TOML
-SEEDS="89752fa7945a06e972d7d860222a5eeaeab5c357@128.140.70.97:26656,dd83e3c7c4e783f8a46dbb010ec8853135d29df0@crossfi-testnet-seed.itrocket.net:36656"
+SEEDS="ade4d8bc8cbe014af6ebdf3cb7b1e9ad36f412c0@testnet-seeds.polkachu.com:30656"
 sed -i.bak -e "s/^seeds =.*/seeds = \"$SEEDS\"/" $CONFIG_TOML
 
-APP_TOML=$HOME/.crossfid/config/app.toml
+APP_TOML=$HOME/.gonative/config/app.toml
 sed -i 's|^pruning *=.*|pruning = "custom"|g' $APP_TOML
 sed -i 's|^pruning-keep-recent  *=.*|pruning-keep-recent = "100"|g' $APP_TOML
 sed -i 's|^pruning-keep-every *=.*|pruning-keep-every = "0"|g' $APP_TOML
@@ -54,27 +56,27 @@ sed -i 's|^pruning-interval *=.*|pruning-interval = "19"|g' $APP_TOML
 sed -i -e "s/^filter_peers *=.*/filter_peers = \"true\"/" $CONFIG_TOML
 indexer="null"
 sed -i -e "s/^indexer *=.*/indexer = \"$indexer\"/" $CONFIG_TOML
-sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "5000000000mpx"|g' $APP_TOML
+#sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.005untiv"|g' $APP_TOML
 
 # Customize ports
-CLIENT_TOML=$HOME/.crossfid/config/client.toml
+CLIENT_TOML=$HOME/.gonative/config/client.toml
 sed -i.bak -e "s/^external_address *=.*/external_address = \"$(wget -qO- eth0.me):$PORT_PPROF_LADDR\"/" $CONFIG_TOML
 sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:$PORT_PROXY_APP\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:$PORT_RPC\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:$PORT_P2P\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:$PORT_PPROF_LADDR\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":$PORT_PROMETHEUS\"%" $CONFIG_TOML && \
-sed -i.bak -e "s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:$PORT_GRPC\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:$PORT_GRPC_WEB\"%; s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:$PORT_API\"%; s%^address = \"127.0.0.1:8545\"%address = \"0.0.0.0:$PORT_EVM_RPC\"%; s%^ws-address = \"127.0.0.1:8546\"%ws-address = \"0.0.0.0:$PORT_EVM_WS\"%; s%^metrics-address = \"127.0.0.1:6065\"%metrics-address = \"0.0.0.0:$PORT_EVM_METRICS\"%" $APP_TOML && \
+sed -i.bak -e "s%^address = \"localhost:9090\"%address = \"0.0.0.0:$PORT_GRPC\"%; s%^address = \"localhost:9091\"%address = \"0.0.0.0:$PORT_GRPC_WEB\"%; s%^address = \"tcp://localhost:1317\"%address = \"tcp://0.0.0.0:$PORT_API\"%; s%^address = \"127.0.0.1:8545\"%address = \"0.0.0.0:$PORT_EVM_RPC\"%; s%^ws-address = \"127.0.0.1:8546\"%ws-address = \"0.0.0.0:$PORT_EVM_WS\"%; s%^metrics-address = \"127.0.0.1:6065\"%metrics-address = \"0.0.0.0:$PORT_EVM_METRICS\"%" $APP_TOML && \
 sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:$PORT_RPC\"%" $CLIENT_TOML
 
 printGreen "Install and configure cosmovisor..." && sleep 1
 
 go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.4.0
-mkdir -p ~/.crossfid/cosmovisor/genesis/bin
-mkdir -p ~/.crossfid/cosmovisor/upgrades
-cp ~/go/bin/crossfid $HOME/.crossfid/cosmovisor/genesis/bin
+mkdir -p ~/.gonative/cosmovisor/genesis/bin
+mkdir -p ~/.gonative/cosmovisor/upgrades
+cp ~/go/bin/gonative $HOME/.gonative/cosmovisor/genesis/bin
 
 printGreen "Starting service and synchronization..." && sleep 1
 
-sudo tee /etc/systemd/system/crossfid.service > /dev/null << EOF
+sudo tee /etc/systemd/system/gonatived.service > /dev/null << EOF
 [Unit]
-Description=CrossFi Node
+Description=Native Network Node
 After=network-online.target
 [Service]
 User=$USER
@@ -82,8 +84,8 @@ ExecStart=$(which cosmovisor) run start
 Restart=on-failure
 RestartSec=3
 LimitNOFILE=10000
-Environment="DAEMON_NAME=crossfid"
-Environment="DAEMON_HOME=$HOME/.crossfid"
+Environment="DAEMON_NAME=gonative"
+Environment="DAEMON_HOME=$HOME/.gonative"
 Environment="DAEMON_ALLOW_DOWNLOAD_BINARIES=false"
 Environment="DAEMON_RESTART_AFTER_UPGRADE=true"
 Environment="UNSAFE_SKIP_BACKUP=true"
@@ -91,16 +93,16 @@ Environment="UNSAFE_SKIP_BACKUP=true"
 WantedBy=multi-user.target
 EOF
 
-crossfid tendermint unsafe-reset-all --home $HOME/.crossfid --keep-addr-book
+gonative tendermint unsafe-reset-all --home $HOME/.gonative --keep-addr-book
 
 # Add snapshot here
-URL="https://snapshots-testnet.stake-town.com/crossfi/crossfi-evm-testnet-1_latest.tar.lz4"
-curl $URL | lz4 -dc - | tar -xf - -C $HOME/.crossfid
-[[ -f $HOME/.crossfid/data/upgrade-info.json ]] && cp $HOME/.crossfid/data/upgrade-info.json $HOME/.crossfid/cosmovisor/genesis/upgrade-info.json
+#URL="https://snapshots-testnet.stake-town.com/crossfi/crossfi-evm-testnet-1_latest.tar.lz4"
+#curl $URL | lz4 -dc - | tar -xf - -C $HOME/.gonative
+#[[ -f $HOME/.gonative/data/upgrade-info.json ]] && cp $HOME/.gonative/data/upgrade-info.json $HOME/.gonative/cosmovisor/genesis/upgrade-info.json
 
 sudo systemctl daemon-reload
-sudo systemctl enable crossfid
-sudo systemctl start crossfid
+sudo systemctl enable gonatived
+sudo systemctl start gonatived
 
 printDelimiter
 printGreen "Check logs:            sudo journalctl -u $BINARY_NAME -f -o cat"
