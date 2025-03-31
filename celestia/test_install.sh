082f6485ed64da372c8eb0b2147363605627a986
yevhensydorenko <mogoll92@gmail.com>
2025-03-31 16:15:46 +0300

    Updated celestia test

--- celestia/test_install.sh
+++ celestia/test_install.sh
@@ -12,7 +12,7 @@ read -r -p "Enter node moniker: " NODE_MONIKER
 CHAIN_ID="mocha-4"
 CHAIN_DENOM="utia"
 BINARY_NAME="celestia-appd"
-BINARY_VERSION_TAG="v3.5.0-mocha"
+BINARY_VERSION_TAG="v3.7.0-mocha"
 CHEAT_SHEET=""
 
 printDelimiter
