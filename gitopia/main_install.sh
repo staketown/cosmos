3a969d7af54dea44f5b444918a116c216b57146b
yevhensydorenko <mogoll92@gmail.com>
2025-09-02 20:52:10 +0300

    Update gitopia

--- gitopia/main_install.sh
+++ gitopia/main_install.sh
@@ -12,7 +12,7 @@ read -r -p "Enter node moniker: " NODE_MONIKER
 CHAIN_ID="gitopia"
 CHAIN_DENOM="ulore"
 BINARY_NAME="gitopiad"
-BINARY_VERSION_TAG="v5.1.0"
+BINARY_VERSION_TAG="v6.0.0"
 
 printDelimiter
 echo -e "Node moniker:       $NODE_MONIKER"
