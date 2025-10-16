3b52e9df73632c6b1315ae04f9ab92bdd39ab58b
yevhensydorenko <mogoll92@gmail.com>
2025-10-16 20:31:49 +0300

    Updated celestia

--- celestia/main_install.sh
+++ celestia/main_install.sh
@@ -12,7 +12,7 @@ read -r -p "Enter node moniker: " NODE_MONIKER
 CHAIN_ID="celestia"
 CHAIN_DENOM="utia"
 BINARY_NAME="celestia-appd"
-BINARY_VERSION_TAG="v5.0.5"
+BINARY_VERSION_TAG="v5.0.11"
 CHEAT_SHEET=""
 
 printDelimiter
